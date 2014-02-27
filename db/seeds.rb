eac1a8da23945e98338b5535543fc1e723562802
David Cornu <davidjcornu@gmail.com>
2014-02-27 16:44:41 +0000

    Populate db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+users = 3.times.map do
+  User.create!(
+    :email => Faker::Internet.email,
+    :login => Faker::Internet.user_name
+  )
+end
+
+stacks = 3.times.map do
+  Stack.create!(
+    :repo_name  => Faker::Internet.domain_name.parameterize,
+    :repo_owner => Faker::Company.name.parameterize
+  )
+end
