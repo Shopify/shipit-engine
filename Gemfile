f9747060ac53dd61e9d632f532989335c097ef15
Dale Hamel <dale.hamel@srvthe.net>
2014-02-27 16:50:12 -0500

    Merged master

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'rails', github: 'rails/rails', branch: '4-1-stable'
-gem 'sqlite3'
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -11,9 +10,17 @@ gem 'resque'
 gem 'unicorn'
 gem 'octokit'
 gem 'faker'
+gem 'capistrano3-unicorn'
+gem 'capistrano-bundler'
+gem 'capistrano-rails'
+
+group :production do
+  gem 'mysql2'
+end
 
 group :development, :test do
   gem 'unicorn-rails'
+  gem 'sqlite3'
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,42 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'shipit2'
+set :repo_url, 'git@shipit2.github.shopify.com:Shopify/shipit2.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/u/apps/shipit2'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml config/secrets.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 50
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:reload'
+  end
+end
