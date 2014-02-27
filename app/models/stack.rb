4cbeb0d449ed992558e3553b924013c20379be6d
Florian Weingarten <flo@hackvalue.de>
2014-02-27 18:14:20 +0000

    deploys test and fixtures

--- app/models/stack.rb
+++ app/models/stack.rb
@@ -4,23 +4,23 @@ class Stack < ActiveRecord::Base
   has_many :commits
   has_many :deploys
 
-  def remote_repo_http_url
+  def repo_http_url
     "https://github.com/#{repo_owner}/#{repo_name}"
   end
 
-  def remote_repo_git_url
+  def repo_git_url
     "git@github.com:#{repo_owner}/#{repo_name}.git"
   end
 
-  def local_base_path
+  def base_path
     File.join(STACKS_PATH, repo_owner, repo_name, environment)
   end
 
-  def local_deploys_path
-    File.join(local_base_path, "deploys")
+  def deploys_path
+    File.join(base_path, "deploys")
   end
 
-  def local_git_path
-    File.join(local_base_path, "git")
+  def git_path
+    File.join(base_path, "git")
   end
 end
--- test/models/stacks_test.rb
+++ test/models/stacks_test.rb
@@ -6,23 +6,23 @@ class StacksTest < ActiveSupport::TestCase
     @expected_base_path = File.join(Rails.root, "shared", "stacks", @stack.repo_owner, @stack.repo_name, @stack.environment)
   end
 
-  test "remote_repo_http_url" do
-    assert_equal "https://github.com/#{@stack.repo_owner}/#{@stack.repo_name}", @stack.remote_repo_http_url
+  test "repo_http_url" do
+    assert_equal "https://github.com/#{@stack.repo_owner}/#{@stack.repo_name}", @stack.repo_http_url
   end
 
-  test "remote_repo_git_url" do
-    assert_equal "git@github.com:#{@stack.repo_owner}/#{@stack.repo_name}.git", @stack.remote_repo_git_url
+  test "repo_git_url" do
+    assert_equal "git@github.com:#{@stack.repo_owner}/#{@stack.repo_name}.git", @stack.repo_git_url
   end
 
-  test "local_base_path" do
-    assert_equal @expected_base_path, @stack.local_base_path
+  test "base_path" do
+    assert_equal @expected_base_path, @stack.base_path
   end
 
-  test "local_deploys_path" do
-    assert_equal File.join(@expected_base_path, "deploys"), @stack.local_deploys_path
+  test "deploys_path" do
+    assert_equal File.join(@expected_base_path, "deploys"), @stack.deploys_path
   end
 
-  test "local_git_path" do
-    assert_equal File.join(@expected_base_path, "git"), @stack.local_git_path
+  test "git_path" do
+    assert_equal File.join(@expected_base_path, "git"), @stack.git_path
   end
 end
