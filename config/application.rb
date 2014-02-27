8376443a8c98571a570a695bb5fc942baa5fa782
Guillaume Malette <gmalette@gmail.com>
2014-02-27 16:47:53 +0000

    Tests

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
---
+++ test/models/users_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UsersTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+end
