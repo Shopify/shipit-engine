c852748db3645e40ea09fcd311c79cf760d3aa4a
David Cornu <davidjcornu@gmail.com>
2014-02-28 19:42:38 +0000

    Show the repo owner if > 1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     @stacks ||= Stack.all
   end
 
+  def stacks_by_owner
+    @stacks_by_owner ||= stacks.group_by(&:repo_owner)
+  end
+
   def is_current_stack?(stack)
     @stack == stack
   end
