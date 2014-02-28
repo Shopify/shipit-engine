9f3276704d1b5c21bbfc5879f5bfa7121c79137e
David Cornu <davidjcornu@gmail.com>
2014-02-28 01:15:34 +0000

    Add commits to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,20 @@ stacks = 3.times.map do
     :repo_owner => Faker::Company.name.parameterize
   )
 end
+
+commits = []
+
+stacks.each do |stack|
+  20.times do
+    user = users.sample
+
+    commits << Commit.create!(
+      :stack_id     => stack.id,
+      :author_id    => user.id,
+      :committer_id => user.id,
+      :sha          => SecureRandom.hex(20),
+      :message      => Faker::Company.catch_phrase,
+      :state        => %w(pending success error failure).sample
+    )
+  end
+end
