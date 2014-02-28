700d9e06cd425ab23cadedba13be2cffa2c41e9b
Guillaume Malette <gmalette@gmail.com>
2014-02-28 20:30:08 +0000

    Adding a ton of http requests to commit fetching

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -4,9 +4,10 @@ class Commit < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :committer, class_name: "User"
 
-  def self.from_github(commit)
+  def self.from_github(commit, state = nil)
     new(
       :sha       => commit.sha,
+      :state     => state,
       :message   => commit.commit.message,
       :author    => User.find_or_create_from_github(commit.author || commit.commit.author),
       :committer => User.find_or_create_from_github(commit.committer || commit.commit.committer),
