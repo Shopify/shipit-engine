35eb5a91047afe3eb11e23105764c49f43f0dace
Jean Boussier <jean.boussier@gmail.com>
2014-02-28 13:55:45 -0500

    A deploy may not have any chunk yet

--- app/helpers/chunks_helper.rb
+++ app/helpers/chunks_helper.rb
@@ -1,5 +1,5 @@
 module ChunksHelper
   def next_chunks_url
-    tail_stack_deploy_chunks_path(@stack, @deploy, last_id: @deploy.chunks.last.id)
+    tail_stack_deploy_chunks_path(@stack, @deploy, last_id: @deploy.chunks.last)
   end
 end
