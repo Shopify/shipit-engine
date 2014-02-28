86bd9abb7014314d5e2c65c5747580b675098036
David Cornu <davidjcornu@gmail.com>
2014-02-28 17:26:16 +0000

    Add stacks to application helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Respond to HTML by default
   respond_to :html
-  
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def stacks
+    @stacks ||= Stacks.all
+  end
 end
