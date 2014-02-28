0b161dec837962586121cbd818bd13cfa30ebeb9
Jean Boussier <jean.boussier@gmail.com>
2014-02-27 20:20:02 -0500

    Use readpartial instead of each_line for non blocking read of commands output

--- lib/command.rb
+++ lib/command.rb
@@ -1,6 +1,7 @@
 require "open3"
 
 class Command
+  MAX_READ = 2 ** 16
   attr_reader :out, :code
 
   def initialize(*args)
@@ -18,9 +19,16 @@ class Command
   def stream(&block)
     _in, @out, wait_thread = Open3.popen2e(*@args)
     _in.close
-    @out.each_line(&block) if block
+    read_stream(@out, &block)
     @code = wait_thread.value
     self
+  end
+
+  def read_stream(io, &block)
+    loop do
+      yield io.readpartial(MAX_READ)
+    end
+  rescue EOFError
   end
 
 end
