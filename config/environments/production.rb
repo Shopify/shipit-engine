24209f0a387a4c7c1ba36bf7d517c139f04ada73
Jean Boussier <jean.boussier@gmail.com>
2014-03-21 10:41:05 -0400

    Use redis for cache and sessions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,8 @@ Shipit::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_store, db: 1
+  config.session_store :redis_store, db: 5
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
