14a7f45bd72944876ab871252696573948d4945e
Jean Boussier <jean.boussier@gmail.com>
2014-03-25 15:23:51 -0400

    Fix force push handling

--- app/jobs/github_sync_job.rb
+++ app/jobs/github_sync_job.rb
@@ -11,6 +11,7 @@ class GithubSyncJob < BackgroundJob
     @stack = Stack.find(params[:stack_id])
 
     new_commits, shared_parent = fetch_missing_commits(@stack.github_commits)
+
     @stack.transaction do
       shared_parent.try(:detach_children!)
       new_commits.each do |gh_commit|
@@ -23,7 +24,7 @@ class GithubSyncJob < BackgroundJob
     commits = []
     iterator = FirstParentCommitsIterator.new(relation)
     iterator.each do |commit|
-      if shared_parent = known?(commit.sha)
+      if shared_parent = lookup_commit(commit.sha)
         return commits.reverse, shared_parent
       end
       commits << commit
@@ -37,8 +38,8 @@ class GithubSyncJob < BackgroundJob
     Shipit.github_api.statuses(@stack.github_repo_name, commit.sha).first
   end
 
-  def known?(sha)
-    @stack.commits.where(sha: sha).exists?
+  def lookup_commit(sha)
+    @stack.commits.find_by_sha(sha)
   end
 
 end
