80e2db3fa7a49258c304b758f954bb6b81113fac
Jean Boussier <jean.boussier@gmail.com>
2014-03-26 16:16:35 -0400

    Fix avatar urls withotu query

--- app/helpers/github_url_helper.rb
+++ app/helpers/github_url_helper.rb
@@ -9,6 +9,7 @@ module GithubUrlHelper
     uri = URI.parse(user.avatar_url) rescue DEFAULT_AVATAR
     attributes = {alt: user.name}
     if options[:size]
+      uri.query ||= ''
       uri.query += "&s=#{options[:size]}"
       attributes[:width] = options[:size]
       attributes[:height] = options[:size]
