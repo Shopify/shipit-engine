fb7f226b8d17d09184b63fe7bb97598389999d91
Jean Boussier <jean.boussier@gmail.com>
2014-04-07 11:38:32 -0400

    Fix commit message HTML escaping
    Fixes #76

--- app/helpers/stacks_helper.rb
+++ app/helpers/stacks_helper.rb
@@ -2,10 +2,12 @@ module StacksHelper
 
   def render_commit_message(commit)
     url = commit.pull_request_url || github_commit_url(commit.stack.repo_owner, commit.stack.repo_name, commit.sha)
-    message = commit.pull_request_title || commit.message
+    message = content_tag(:span, commit.pull_request_title || commit.message, class: 'event-message')
+
     github_id = commit.pull_request? ? "##{commit.pull_request_id}" : commit.short_sha
-    message += ' ' + content_tag(:span, "(#{github_id})", class: 'event-number')
-    link_to(message.html_safe, url, target: '_blank')
+    identifier = content_tag(:span, "(#{github_id})", class: 'event-number').to_s.html_safe
+
+    link_to(message + identifier, url, target: '_blank')
   end
 
 end
