330e8013e31aaa6bba1d66b26f16ad149883652c
Guillaume Malette <gmalette@gmail.com>
2014-05-06 10:09:06 -0400

    Verify Github webhook signatures

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -1,3 +1,8 @@
 class Webhook < ActiveRecord::Base
   belongs_to :stack
+
+  def verify_signature(signature, message)
+    algorithm, signature = signature.split("=", 2)
+    signature == OpenSSL::HMAC.hexdigest(algorithm, secret, message)
+  end
 end
--- test/controllers/webhooks_controller_test.rb
+++ test/controllers/webhooks_controller_test.rb
@@ -8,18 +8,28 @@ class WebhooksControllerTest < ActionController::TestCase
   test ":push with the target branch queues a job" do
     Resque.expects(:enqueue).with(GithubSyncJob, stack_id: @stack.id)
     Resque.expects(:enqueue).with(GitMirrorUpdateJob, stack_id: @stack.id)
+
+    Webhook.any_instance.expects(:verify_signature).returns(true)
+
+    request.headers['X-Github-Event'] = 'push'
     params = {"ref"=>"refs/heads/master", "after"=>"79d29e99cb83e0ba16c9e30c502a60995e711e5f", "before"=>"16c259864de2fc2da2e185c3088f4f33e5b09a3c", "created"=>false, "deleted"=>false, "forced"=>false, "compare"=>"https://github.com/byroot/junk/compare/16c259864de2...79d29e99cb83", "commits"=>[{"id"=>"79d29e99cb83e0ba16c9e30c502a60995e711e5f", "distinct"=>true, "message"=>"modif", "timestamp"=>"2014-02-28T10:27:42-08:00", "url"=>"https://github.com/byroot/junk/commit/79d29e99cb83e0ba16c9e30c502a60995e711e5f", "author"=>{"name"=>"Guillaume Malette", "email"=>"gmalette@gmail.com", "username"=>"gmalette"}, "committer"=>{"name"=>"Guillaume Malette", "email"=>"gmalette@gmail.com", "username"=>"gmalette"}, "added"=>nil, "removed"=>nil, "modified"=>["toto.txt"]}], "head_commit"=>{"id"=>"79d29e99cb83e0ba16c9e30c502a60995e711e5f", "distinct"=>true, "message"=>"modif", "timestamp"=>"2014-02-28T10:27:42-08:00", "url"=>"https://github.com/byroot/junk/commit/79d29e99cb83e0ba16c9e30c502a60995e711e5f", "author"=>{"name"=>"Guillaume Malette", "email"=>"gmalette@gmail.com", "username"=>"gmalette"}, "committer"=>{"name"=>"Guillaume Malette", "email"=>"gmalette@gmail.com", "username"=>"gmalette"}, "added"=>nil, "removed"=>nil, "modified"=>["toto.txt"]}, "repository"=>{"id"=>17266426, "name"=>"junk", "url"=>"https://github.com/byroot/junk", "description"=>"Pure test repo, look elsewhere please.", "watchers"=>0, "stargazers"=>0, "forks"=>0, "fork"=>false, "size"=>0, "owner"=>{"name"=>"byroot", "email"=>"jean.boussier@gmail.com"}, "private"=>false, "open_issues"=>0, "has_issues"=>true, "has_downloads"=>true, "has_wiki"=>true, "created_at"=>1393538266, "pushed_at"=>1393612064, "master_branch"=>"master"}, "pusher"=>{"name"=>"gmalette", "email"=>"gmalette@gmail.com"}, "webhook"=>{}}
     post :push, { stack_id: @stack.id }.merge(params)
   end
 
   test ":push does not enqueue a job if not the target branch" do
     Resque.expects(:enqueue).never
+    Webhook.any_instance.expects(:verify_signature).returns(true)
+
+    request.headers['X-Github-Event'] = 'push'
     params = {"ref"=>"refs/heads/not-master", "after"=>"79d29e99cb83e0ba16c9e30c502a60995e711e5f", "before"=>"16c259864de2fc2da2e185c3088f4f33e5b09a3c", "created"=>false, "deleted"=>false, "forced"=>false, "compare"=>"https://github.com/byroot/junk/compare/16c259864de2...79d29e99cb83", "commits"=>[{"id"=>"79d29e99cb83e0ba16c9e30c502a60995e711e5f", "distinct"=>true, "message"=>"modif", "timestamp"=>"2014-02-28T10:27:42-08:00", "url"=>"https://github.com/byroot/junk/commit/79d29e99cb83e0ba16c9e30c502a60995e711e5f", "author"=>{"name"=>"Guillaume Malette", "email"=>"gmalette@gmail.com", "username"=>"gmalette"}, "committer"=>{"name"=>"Guillaume Malette", "email"=>"gmalette@gmail.com", "username"=>"gmalette"}, "added"=>nil, "removed"=>nil, "modified"=>["toto.txt"]}], "head_commit"=>{"id"=>"79d29e99cb83e0ba16c9e30c502a60995e711e5f", "distinct"=>true, "message"=>"modif", "timestamp"=>"2014-02-28T10:27:42-08:00", "url"=>"https://github.com/byroot/junk/commit/79d29e99cb83e0ba16c9e30c502a60995e711e5f", "author"=>{"name"=>"Guillaume Malette", "email"=>"gmalette@gmail.com", "username"=>"gmalette"}, "committer"=>{"name"=>"Guillaume Malette", "email"=>"gmalette@gmail.com", "username"=>"gmalette"}, "added"=>nil, "removed"=>nil, "modified"=>["toto.txt"]}, "repository"=>{"id"=>17266426, "name"=>"junk", "url"=>"https://github.com/byroot/junk", "description"=>"Pure test repo, look elsewhere please.", "watchers"=>0, "stargazers"=>0, "forks"=>0, "fork"=>false, "size"=>0, "owner"=>{"name"=>"byroot", "email"=>"jean.boussier@gmail.com"}, "private"=>false, "open_issues"=>0, "has_issues"=>true, "has_downloads"=>true, "has_wiki"=>true, "created_at"=>1393538266, "pushed_at"=>1393612064, "master_branch"=>"master"}, "pusher"=>{"name"=>"gmalette", "email"=>"gmalette@gmail.com"}, "webhook"=>{}}
     post :push, { stack_id: @stack.id }.merge(params)
   end
 
   test ":state updates the commit with the payload state" do
+    Webhook.any_instance.expects(:verify_signature).returns(true)
     commit = commits(:first)
+
+    request.headers['X-Github-Event'] = 'status'
     params = {"sha" => commit.sha, "state" => "pending", "target_url" => "https://ci.example.com/1000/output"}
     post :state, { stack_id: @stack.id }.merge(params)
 
@@ -29,6 +39,9 @@ class WebhooksControllerTest < ActionController::TestCase
   end
 
   test ":state with a unexisting commit trows ActiveRecord::RecordNotFound" do
+    Webhook.any_instance.expects(:verify_signature).returns(true)
+
+    request.headers['X-Github-Event'] = 'status'
     params = {"sha" => "notarealcommit", "state" => "pending"}
     assert_raises ActiveRecord::RecordNotFound do
       post :state, { stack_id: @stack.id }.merge(params)
@@ -36,7 +49,7 @@ class WebhooksControllerTest < ActionController::TestCase
   end
 
   test ":push returns head :ok if request is ping" do
-    @request.env['HTTP_X_GITHUB_EVENT'] = 'ping'
+    @request.headers['X-Github-Event'] = 'ping'
 
     Resque.expects(:enqueue).never
     post :state, { stack_id: @stack.id, zen: "Git is beautiful" }
@@ -44,11 +57,39 @@ class WebhooksControllerTest < ActionController::TestCase
   end
 
   test ":state returns head :ok if request is ping" do
-    @request.env['HTTP_X_GITHUB_EVENT'] = 'ping'
+    @request.headers['X-Github-Event'] = 'ping'
 
     commit = commits(:first)
     post :state, { stack_id: @stack.id }
     Resque.expects(:enqueue).never
     assert_response :ok
+  end
+
+  test ":state verifies webhook signature" do
+    commit = commits(:first)
+
+    params = {"sha" => commit.sha, "state" => "pending", "target_url" => "https://ci.example.com/1000/output"}
+    signature = 'sha1=4848deb1c9642cd938e8caa578d201ca359a8249'
+
+    @request.headers['X-Github-Event'] = 'push'
+    @request.headers['X-Hub-Signature'] = signature
+
+    Webhook.any_instance.expects(:verify_signature).with(signature, URI.encode_www_form(params)).returns(false)
+
+    post :push, { stack_id: @stack.id }.merge(params)
+    assert_response :unprocessable_entity
+  end
+
+  test ":push verifies webhook signature" do
+    params = {"ref"=>"refs/heads/master"}
+    signature = 'sha1=ad1d939e9acd6bdc2415a2dd5951be0f2a796ce0'
+
+    @request.headers['X-Github-Event'] = 'push'
+    @request.headers['X-Hub-Signature'] = signature
+
+    Webhook.any_instance.expects(:verify_signature).with(signature, URI.encode_www_form(params)).returns(false)
+
+    post :push, { stack_id: @stack.id }.merge(params)
+    assert_response :unprocessable_entity
   end
 end
