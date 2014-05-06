43b194a4e8c57fa1844c25d63d0916a34b92e410
Guillaume Malette <gmalette@gmail.com>
2014-05-06 10:16:02 -0400

    Regenerate all webhooks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506025446) do
+ActiveRecord::Schema.define(version: 20140506140931) do
 
   create_table "commits", force: true do |t|
     t.integer  "stack_id",                                    null: false
