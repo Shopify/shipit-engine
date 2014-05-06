bb15bdc534cc93e24926c81818cb50db4b8e96fa
Guillaume Malette <gmalette@gmail.com>
2014-05-06 13:26:36 -0400

    Fix teardown migration

--- db/migrate/20140506140931_regenerate_all_webhooks.rb
+++ db/migrate/20140506140931_regenerate_all_webhooks.rb
@@ -1,7 +1,7 @@
 class RegenerateAllWebhooks < ActiveRecord::Migration
   def up
     Stack.find_each do |stack|
-      GithubTeardownWebhooksJob.new.perform(stack_id: stack.id)
+      GithubTeardownWebhooksJob.new.perform(stack_id: stack.id, github_repo_name: stack.github_repo_name)
       GithubSetupWebhooksJob.new.perform(stack_id: stack.id)
     end
   end
