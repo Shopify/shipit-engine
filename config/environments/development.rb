98259e798726a8abdd65eb54779c044d7c11f4c1
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-05-10 17:29:42 -0400

    security: reset_session on authentication to prevent session fixation.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Shipit::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.session_store :redis_store, {db: 2}
 end
