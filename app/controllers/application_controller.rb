e3e5cf7d54712cc0966b513c365139615b2d9503
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-05-10 17:28:32 -0400

    Remove unused return_to session value.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   def authenticate
     auth_settings = Settings.authentication
     return if session[:authenticated] || auth_settings.blank?
-    session[:return_to] = request.fullpath
     redirect_to authentication_path(provider: auth_settings.provider, origin: request.fullpath)
   rescue Settingslogic::MissingSetting
   end
