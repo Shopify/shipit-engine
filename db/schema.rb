0420f4350d3fc308da4276ba1e9953f6b7a0350f
Jean Boussier <jean.boussier@gmail.com>
2014-05-21 15:40:18 -0400

    Clear deploys working directory once completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520150101) do
+ActiveRecord::Schema.define(version: 20140521193738) do
 
   create_table "commits", force: true do |t|
     t.integer  "stack_id",                                    null: false
