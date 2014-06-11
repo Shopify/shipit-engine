6d75a0c75ae0909ec2aba5e2aae34a555d553b12
Jean Boussier <jean.boussier@gmail.com>
2014-06-11 11:18:33 -0400

    Revert "Disabled chunks rollup"
    This reverts commit a7e804a46330a8252e782b69c3c228a92a95a0e7.

--- app/models/deploy.rb
+++ app/models/deploy.rb
@@ -35,7 +35,7 @@ class Deploy < ActiveRecord::Base
     state :success
     state :error
 
-    # after_transition from: :running, do: :rollup_chunks # FIXME: I suspect this to be the reason of the MySQL deadlocks
+    after_transition from: :running, do: :rollup_chunks
     after_transition :broadcast_deploy
     after_transition to: :success, do: :schedule_continuous_delivery
   end
