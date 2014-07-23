c5787d06b135c15e5a4bdd7310a0911497c76ae7
Hammad Khalid <hammad.khalid@shopify.com>
2014-07-23 14:31:08 +0000

    Add a field in the settings view page for reminder URL.

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -72,6 +72,6 @@ class StacksController < ApplicationController
   end
 
   def update_params
-    params.require(:stack).permit(:checklist, :deploy_url, :deploys_count, :lock_reason, :continuous_deployment)
+    params.require(:stack).permit(:checklist, :deploy_url, :deploys_count, :lock_reason, :continuous_deployment, :shipit_reminder, :reminder_url)
   end
 end
