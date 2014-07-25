c33be87fc6d5ad0220d0be1db0c300b64cb23f1a
Hammad Khalid <hammad.khalid@shopify.com>
2014-07-25 19:51:24 +0000

    Allow reminder_url to be updated via views through the Stack controller.

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -72,6 +72,6 @@ class StacksController < ApplicationController
   end
 
   def update_params
-    params.require(:stack).permit(:checklist, :deploy_url, :deploys_count, :lock_reason, :continuous_deployment, :shipit_reminder, :reminder_url)
+    params.require(:stack).permit(:checklist, :deploy_url, :deploys_count, :lock_reason, :continuous_deployment, :reminder_url)
   end
 end
