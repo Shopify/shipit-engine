767f4c03e1bcdde0de6a43e07ae88a356d92aa29
phoet <phoetmail@googlemail.com>
2014-08-19 12:48:12 +0200

    this is now a hash

--- config/initializers/authentication.rb
+++ config/initializers/authentication.rb
@@ -1,18 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  auth_config = Shipit.authentication
-
-  if Shipit.authentication.present?
-    parameters = if auth_config.respond_to?(:options)
-      [auth_config.options]
-    elsif auth_config.respond_to?(:parameters)
-      Array.wrap(auth_config.parameters)
-    else
-      []
-    end
-    provider(auth_config.provider, *parameters)
+  if auth_config = Shipit.authentication
+    parameters = Array(auth_config['parameters'] || auth_config['options'])
+    provider(auth_config['provider'], *parameters)
   end
 
-  if Shipit.github
-    provider :github, Shipit.github_key, Shipit.github_secret, scope: 'email'
+  if github_config = Shipit.github
+    provider :github, github_config['key'], github_config['secret'], scope: 'email'
   end
 end
