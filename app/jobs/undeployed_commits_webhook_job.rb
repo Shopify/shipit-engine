3fdf12df23a1b403ccad9f814677b7d9e9b30e57
Hammad Khalid <hammad.khalid@shopify.com>
2014-08-20 03:28:42 +0000

    Update method name to be more explicit

--- app/jobs/undeployed_commits_webhook_job.rb
+++ app/jobs/undeployed_commits_webhook_job.rb
@@ -10,11 +10,11 @@ class UndeployedCommitsWebhookJob < BackgroundJob
 
     if old_undeployed_commits.present?
       committer_ids = old_undeployed_commits.pluck(:committer_id).uniq
-      send_reminder(build_stack_committer_info(stack, committer_ids), stack.reminder_url)
+      send_reminder(build_stack_committer_json(stack, committer_ids), stack.reminder_url)
     end
   end
 
-  def build_stack_committer_info(stack, committer_ids)
+  def build_stack_committer_json(stack, committer_ids)
     stack_committer_info                = {}
     stack_committer_info[:repo_name]    = stack.repo_name
     stack_committer_info[:repo_branch]  = stack.branch
@@ -22,13 +22,13 @@ class UndeployedCommitsWebhookJob < BackgroundJob
     stack_committer_info.to_json
   end
 
-  def send_reminder(stack_committer_info, reminder_url)
+  def send_reminder(stack_committer_json, reminder_url)
     max_retries = 3
 
     begin
       response = Net::HTTP.post_form(
         URI.parse(reminder_url),
-        {"stack_committer_json" => stack_committer_info }
+        {"stack_committer_json" => stack_committer_json }
       )
     rescue Timeout::Error, Errno::ETIMEDOUT
       retry if (max_retries -= 1) > 0
