4fc2e03866c4d9221a77a853e187d642956a1fe2
Jean Boussier <jean.boussier@gmail.com>
2015-01-07 17:09:47 -0500

    Use PTY.spawn instead of Open3.popen2 to have capistrano timestamps and colors

--- test/unit/command_test.rb
+++ test/unit/command_test.rb
@@ -4,7 +4,7 @@ class CommandTest < ActiveSupport::TestCase
   test 'lib/snippets is added to the PATH when command is run' do
     out = Command.new('which extract-gem-version', chdir: '/tmp').run
     script_path = Rails.root.join('lib', 'snippets', 'extract-gem-version').to_s
-    assert_equal "#{script_path}\n", out
+    assert_equal "#{script_path}\r\n", out
   end
 
   test "#interpolate_environment_variables replace environment variables by their value" do
