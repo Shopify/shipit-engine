bb48904804f15d0ebca8258d714f2a96e069fba9
Jean Boussier <jean.boussier@gmail.com>
2015-01-21 16:15:28 -0500

    Enable word array cop and fix a few tests

--- test/controllers/chunks_controller_test.rb
+++ test/controllers/chunks_controller_test.rb
@@ -11,18 +11,18 @@ class ChunksControllerTest < ActionController::TestCase
   test ":index is success" do
     get :index, stack_id: @stack.to_param, task_id: @deploy.id, format: :json
     assert_response :success
-    assert_equal @response.body, @deploy.chunks.to_json
+    assert_equal @deploy.chunks.to_json, @response.body
   end
 
   test ":index with last_id" do
     get :index, stack_id: @stack.to_param, task_id: @deploy.id, last_id: @last_chunk.id, format: :json
     assert_response :success
-    assert_equal @response.body, '[]'
+    assert_equal '[]', @response.body
   end
 
   test ":tail" do
     get :tail, stack_id: @stack.to_param, task_id: @deploy.id, last_id: @last_chunk.id, format: :json
     assert_response :success
-    assert_equal JSON.parse(@response.body).keys, ['url', 'task', 'chunks']
+    assert_equal %w(url task chunks), JSON.parse(@response.body).keys
   end
 end
