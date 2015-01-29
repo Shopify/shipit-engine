bf07de96ae87048e8e1edf543c0cbbb9dfa04932
Jean Boussier <jean.boussier@gmail.com>
2015-01-29 16:36:45 -0500

    Reject users without login from the refresh batches

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   end
 
   def self.refresh_shard(shard_index, shards_count)
-    where('id % ? = ?', shards_count, shard_index).find_each do |user|
+    where.not(login: nil).where('id % ? = ?', shards_count, shard_index).find_each do |user|
       Resque.enqueue(RefreshGithubUserJob, user_id: user.id)
     end
   end
