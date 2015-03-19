caed74ddcd50ccae113ec35a44f21ccd6b0f7e89
Jean Boussier <jean.boussier@gmail.com>
2015-03-19 18:53:54 -0400

    Improve commit message formatting in API responses

--- app/serializers/short_commit_serializer.rb
+++ app/serializers/short_commit_serializer.rb
@@ -1,3 +1,7 @@
 class ShortCommitSerializer < ActiveModel::Serializer
   attributes :sha, :message
+
+  def message
+    object.pull_request? ? "#{object.pull_request_title} (##{object.pull_request_id})" : object.message
+  end
 end
