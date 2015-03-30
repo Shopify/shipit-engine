e68fcc69cc1e0d69e558bbeddf4b32405274bf8c
Jean Boussier <jean.boussier@gmail.com>
2015-03-30 11:20:56 -0400

    Disable bugsnag reporting in dev/test environments

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,5 @@
-Bugsnag.configure do |config|
-  config.api_key = '8f5ef714c28f7ea7b5c1fde664d3dc7a'
+unless Rails.application.config.consider_all_requests_local
+  Bugsnag.configure do |config|
+    config.api_key = '8f5ef714c28f7ea7b5c1fde664d3dc7a'
+  end
 end
