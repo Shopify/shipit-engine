e031b8846433ad9ce73133725cbd600ffc1fbaf4
Paul Robertson <t.paulrobertson@gmail.com>
2015-10-23 18:35:58 -0400

    Set up database defaults

--- template.rb
+++ template.rb
@@ -44,6 +44,29 @@ web: bundle exec rails s -p $PORT
 worker: bundle exec sidekiq -C config/sidekiq.yml
 CODE
 
+remove_file 'config/database.yml'
+create_file 'config/database.yml', <<-CODE
+default: &default
+  adapter: sqlite3
+  pool: 5
+  timeout: 5000
+
+development:
+  <<: *default
+  database: db/development.sqlite3
+
+# Warning: The database defined as "test" will be erased and
+# re-generated from your development database when you run "rake".
+# Do not set this db to the same as development or production.
+test:
+  <<: *default
+  database: db/test.sqlite3
+
+production:
+  url:  <%= ENV['DATABASE_URL'] %>
+  pool: <%= ENV['DB_POOL'] || 5 %>
+CODE
+
 create_file 'config/sidekiq.yml', <<-CODE
 :concurrency: 1
 :queues:
@@ -112,30 +135,6 @@ if yes?("Are you hosting Shipit on Heroku? (y/n)")
   gem_group :development, :test do
     gem 'sqlite3'
   end
-
-  remove_file 'config/database.yml'
-  create_file 'config/database.yml', <<-EOF
-default: &default
-  adapter: sqlite3
-  pool: 5
-  timeout: 5000
-
-development:
-  <<: *default
-  database: db/development.sqlite3
-
-# Warning: The database defined as "test" will be erased and
-# re-generated from your development database when you run "rake".
-# Do not set this db to the same as development or production.
-test:
-  <<: *default
-  database: db/test.sqlite3
-
-production:
-  url:  <%= ENV['DATABASE_URL'] %>
-  pool: <%= ENV['DB_POOL'] || 5 %>
-
-EOF
 end
 
 after_bundle do
