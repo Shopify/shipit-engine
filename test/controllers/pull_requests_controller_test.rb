068feec6017db8ee3964f35a1427b4990cb40b7f
Jean Boussier <jean.boussier@gmail.com>
2017-02-16 11:18:33 +0100

    Add form to request a PR merge

--- test/controllers/pull_requests_controller_test.rb
+++ test/controllers/pull_requests_controller_test.rb
@@ -14,6 +14,13 @@ module Shipit
       assert_select '.pr-list .pr', @stack.pull_requests.pending.count
     end
 
+    test "#add can enqueue a pull request" do
+      assert_difference -> { PullRequest.count }, +1 do
+        post :create, params: {stack_id: @stack.to_param, number_or_url: '#5'}
+      end
+      assert_redirected_to stack_pull_requests_path(@stack)
+    end
+
     test "#destroy can cancel a pending pull request" do
       assert_predicate @pr, :pending?
       delete :destroy, params: {stack_id: @stack.to_param, id: @pr.id}
