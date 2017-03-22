dceecbc1435ee057596293052541a456b161fc73
Przemysław Dąbek <przemyslaw.dabek@uxpin.com>
2017-03-23 00:08:46 +0100

    more explicit removal of dependent records when destroying a stack

--- app/jobs/shipit/destroy_stack_job.rb
+++ app/jobs/shipit/destroy_stack_job.rb
@@ -2,7 +2,32 @@ module Shipit
   class DestroyStackJob < BackgroundJob
     queue_as :default
 
+    # stack
+    # +-- api_clients
+    # +-- commits
+    # |   +-- commit_deployments
+    # |   |   +-- statuses
+    # |   +-- statuses
+    # +-- github_hooks
+    # +-- hooks
+    # +-- pull_requests
+    # +-- tasks
+    #     +-- chunks
+
     def perform(stack)
+      Shipit::ApiClient.where(stack_id: stack.id).delete_all
+      commits_ids = Shipit::Commit.where(stack_id: stack.id).pluck(:id)
+      commit_deployments_ids = Shipit::CommitDeployment.where(commit_id: commits_ids).pluck(:id)
+      Shipit::CommitDeploymentStatus.where(commit_deployment_id: commit_deployments_ids).delete_all
+      Shipit::CommitDeployment.where(id: commit_deployments_ids).delete_all
+      Shipit::Status.where(commit_id: commits_ids).delete_all
+      Shipit::Commit.where(id: commits_ids).delete_all
+      Shipit::GithubHook.where(stack_id: stack.id).destroy_all
+      Shipit::Hook.where(stack_id: stack.id).delete_all
+      Shipit::PullRequest.where(stack_id: stack.id).delete_all
+      tasks_ids = Shipit::Task.where(stack_id: stack.id).pluck(:id)
+      Shipit::OutputChunk.where(task_id: tasks_ids).delete_all
+      Shipit::Task.where(id: tasks_ids).delete_all
       stack.destroy!
     end
   end
