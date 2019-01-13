87fd223be138da58b59a6691a94143bb5aaeda48
Richard McGain <richard.mcgain@shopify.com>
2019-01-14 10:13:52 +1100

    Show an error message for the title when it would otherwise result in a 500

--- test/models/tasks_test.rb
+++ test/models/tasks_test.rb
@@ -12,5 +12,10 @@ module Shipit
       task = shipit_tasks(:shipit_restart)
       assert_equal 'Restart application', task.title
     end
+
+    test '#title returns an error message when the title raises an error' do
+      task = shipit_tasks(:shipit_with_title_parsing_issue)
+      assert_equal 'This task (title: Using the %{WRONG_VARIABLE_NAME}) cannot be shown due to an incorrect variable name. Check your shipit.yml file', task.title
+    end
   end
 end
