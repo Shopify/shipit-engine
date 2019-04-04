be0eb51d6b41bd008d437aee168997fd49536d47
Jack Li <jack.teng.li@gmail.com>
2019-04-04 11:52:39 -0400

    Add ui for managing api clients

---
+++ app/controllers/shipit/api_clients_controller.rb
@@ -0,0 +1,49 @@
+module Shipit
+  class ApiClientsController < ShipitController
+    include Pagination
+
+    def index
+      paginator = paginate(ApiClient.order(created_at: :desc).all)
+      @api_clients = paginator.to_a
+      @links = paginator.links
+    end
+
+    def new
+      @api_client = ApiClient.new
+    end
+
+    def create
+      @api_client = ApiClient.new(create_params.merge(creator_id: current_user.id))
+      unless @api_client.save
+        flash[:warning] = @api_client.errors.full_messages.to_sentence
+      end
+
+      respond_with(@api_client)
+    end
+
+    def show
+      @api_client = ApiClient.find(params[:id])
+    end
+
+    def update
+      @api_client = ApiClient.find(params[:id])
+      if @api_client.update(update_params)
+        options = {flash: {success: 'Successfully updated'}}
+      else
+        options = {flash: {warning: @stack.errors.full_messages.to_sentence}}
+      end
+
+      redirect_to(params[:return_to].presence || api_client_path(@api_client), options)
+    end
+
+    private
+
+    def create_params
+      params.require(:api_client).permit(:name, :permissions => [])
+    end
+
+    def update_params
+      params.require(:api_client).permit(:permissions => [])
+    end
+  end
+end
