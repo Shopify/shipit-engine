0673deae8ccbbe0dc5b0f45405ec461b234606dc
Jean Boussier <jean.boussier@gmail.com>
2019-09-13 00:08:14 +0200

    Update setup for Rails 6.0

--- template.rb
+++ template.rb
@@ -1,10 +1,10 @@
 # Template for rails new app
 # Run this like `rails new shipit -m template.rb`
-if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.3')
-  raise Thor::Error, "You need at least Ruby 2.3 to install shipit"
+if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.5')
+  raise Thor::Error, "You need at least Ruby 2.5 to install shipit"
 end
-if Gem::Version.new(Rails::VERSION::STRING) < Gem::Version.new('5.2')
-  raise Thor::Error, "You need Rails 5.2 to install shipit"
+if Gem::Version.new(Rails::VERSION::STRING) < Gem::Version.new('6.0')
+  raise Thor::Error, "You need Rails 6.0 to install shipit"
 end
 
 route %(mount Shipit::Engine, at: '/')
@@ -115,7 +115,7 @@ Sidekiq.configure_client do |config|
 end
 CODE
 
-inject_into_file 'config/application.rb', after: "load_defaults 5.2\n" do
+inject_into_file 'config/application.rb', after: "load_defaults 6.0\n" do
   "\n    config.active_job.queue_adapter = :sidekiq\n"
 end
 
