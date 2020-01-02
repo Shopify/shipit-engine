442cd4fd71ef98efdcebc827c1f09591b9fefedb
Ben Langfeld <blangfeld@powerhrg.com>
2020-01-02 15:56:51 -0300

    Makes Task commit association optional
    Tasks do not need to be associated with particular commits; it is sometimes desirable to programatically create a Stack and a Task in immediate succession, prior to the GithubSyncJob having executed, to perform a provisioning step which must execute prior to a project-defined deployment; for this kind of task, an association with a commit range is not meaningful or useful.

---
+++ db/migrate/20200102175621_optional_task_commits.rb
@@ -0,0 +1,6 @@
+class OptionalTaskCommits < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null :tasks, :since_commit_id, true
+    change_column_null :tasks, :until_commit_id, true
+  end
+end
