e90f024918679be549709ce47f2d54b9ba51829f
Aaron Kuehler <aaron.kuehler@gmail.com>
2020-01-06 10:56:05 -0500

    Define webhook class methods by opening the singleton class

--- lib/shipit/webhooks.rb
+++ lib/shipit/webhooks.rb
@@ -2,31 +2,33 @@ require 'shipit/webhooks/handlers'
 
 module Shipit
   module Webhooks
-    def self.default_handlers
-      {
-        'push' => [Handlers::PushHandler],
-        'status' => [Handlers::StatusHandler],
-        'membership' => [Handlers::MembershipHandler],
-        'check_suite' => [Handlers::CheckSuiteHandler],
-      }
-    end
+    class << self
+      def default_handlers
+        {
+          'push' => [Handlers::PushHandler],
+          'status' => [Handlers::StatusHandler],
+          'membership' => [Handlers::MembershipHandler],
+          'check_suite' => [Handlers::CheckSuiteHandler],
+        }
+      end
 
-    def self.handlers
-      @handlers ||= reset_handler_registry
-    end
+      def handlers
+        @handlers ||= reset_handler_registry
+      end
 
-    def self.reset_handler_registry
-      @handlers = default_handlers
-    end
+      def reset_handler_registry
+        @handlers = default_handlers
+      end
 
-    def self.register_handler(event, callable = nil, &block)
-      handlers[event] ||= []
-      handlers[event] << callable if callable
-      handlers[event] << block if block_given?
-    end
+      def register_handler(event, callable = nil, &block)
+        handlers[event] ||= []
+        handlers[event] << callable if callable
+        handlers[event] << block if block_given?
+      end
 
-    def self.for_event(event)
-      handlers.fetch(event) { [] }
+      def for_event(event)
+        handlers.fetch(event) { [] }
+      end
     end
   end
 end
