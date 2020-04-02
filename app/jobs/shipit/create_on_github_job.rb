55fd47ceb195ae7cab46e16edc7222ba8ebfe018
Ben Langfeld <blangfeld@powerhrg.com>
2020-04-02 19:21:39 -0300

    Reload records to be created on Github
    This prevents us from duplicating data on Github.
    
    See https://github.com/Shopify/shipit-engine/pull/1042#issuecomment-608118022

--- app/jobs/shipit/create_on_github_job.rb
+++ app/jobs/shipit/create_on_github_job.rb
@@ -3,13 +3,14 @@ module Shipit
     include BackgroundJob::Unique
 
     queue_as :default
+    on_duplicate :drop
 
     # We observe that some objects regularly take longer than the default 10 seconds to create, e.g. deployments
     self.timeout = 40
     self.lock_timeout = 20
 
     def perform(record)
-      record.create_on_github!
+      record.reload.create_on_github!
     end
   end
 end
