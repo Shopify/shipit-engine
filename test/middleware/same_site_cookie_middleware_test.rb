46d570afe7956f7286d0abba1dd229b79cef1526
Alex Taylor <alex.taylor@clio.com>
2020-04-14 14:28:38 -0700

    Add test to cover multiple cookies being set

--- test/middleware/same_site_cookie_middleware_test.rb
+++ test/middleware/same_site_cookie_middleware_test.rb
@@ -7,6 +7,7 @@ module Shipit
         response = Rack::Response.new("", 200, "Content-Type" => "text/yaml")
 
         response.set_cookie("session_test", value: "session_test", domain: ".test.com", path: "/")
+        response.set_cookie("another_cookie", value: "123abc", domain: ".test.com", path: "/")
         response.finish
       })
     end
@@ -26,6 +27,17 @@ module Shipit
       _status, headers, _body = middleware.call(env)
 
       assert_includes headers['Set-Cookie'], 'SameSite'
+    end
+
+    test 'SameSite cookie attributes should be added when multiple cookies are present' do
+      env = env_for_url("https://test.com/")
+
+      _status, headers, _body = middleware.call(env)
+
+      session_cookie, other_cookie = headers['Set-Cookie'].split("\n")
+
+      assert_includes session_cookie, 'SameSite'
+      assert_includes other_cookie, 'SameSite'
     end
 
     test 'SameSite cookie attributes should not be added on non SSL requests' do
