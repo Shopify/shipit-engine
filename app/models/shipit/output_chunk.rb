6fce3c06eeb99ef70e0a005e608638dedf35b058
Darren Worrall <darren.worrall@shopify.com>
2020-04-29 13:49:21 +0100

    Fix frozen string error

--- app/models/shipit/output_chunk.rb
+++ app/models/shipit/output_chunk.rb
@@ -6,7 +6,11 @@ module Shipit
     scope :tail, ->(start) { order(id: :asc).where('id > ?', start || 0) }
 
     def text=(string)
-      super(string.force_encoding(Encoding::UTF_8).scrub)
+      if string.frozen?
+        super(string)
+      else
+        super(string.force_encoding(Encoding::UTF_8).scrub)
+      end
     end
   end
 end
