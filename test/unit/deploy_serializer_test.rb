e9f1315d360d104baeed097c2b4ddbd1202da13d
Vinicius Gama <vinicius.sgama@gmail.com>
2020-07-28 16:00:28 -0300

    Expose issue with a test

---
+++ test/unit/deploy_serializer_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+module Shipit
+  class DeploySerializerTest < ActiveSupport::TestCase
+    test 'includes author object' do
+      deploy = shipit_deploys(:shipit)
+      first_commit_author = deploy.commits.first.author
+
+      serializer = ActiveModel::Serializer.serializer_for(deploy)
+      assert_equal DeploySerializer, serializer
+      serialized = serializer.new(deploy).to_json
+
+      assert_json("commits.0.author.name", first_commit_author.name, document: serialized)
+    end
+  end
+end
