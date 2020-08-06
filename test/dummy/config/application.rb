94716335e6f2617e74dd34b3ca9edc7d59d86761
Jean Boussier <jean.boussier@gmail.com>
2020-08-06 14:53:00 +0200

    Avoid loading useless parts of Rails in development

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'active_job/railtie'
+require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 Bundler.require(*Rails.groups)
 require "shipit"
