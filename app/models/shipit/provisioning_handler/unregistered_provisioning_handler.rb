0af0c0b7522dd7808e06896c6b72e1cf16d97fba
Aaron Kuehler <aaron.kuehler@gmail.com>
2020-09-28 15:20:30 -0400

    Document the relevance of throw(:halt) in provisoining handler

--- app/models/shipit/provisioning_handler/unregistered_provisioning_handler.rb
+++ app/models/shipit/provisioning_handler/unregistered_provisioning_handler.rb
@@ -19,6 +19,15 @@ module Shipit
           "ProvisioningHandler registry. Have you registered it via Provisioning::Handler.register?",
           Shipit::AnonymousUser.new
         )
+
+        # Prevent transition of the ReviewStack 'provision_status'
+        # state machine. This signals to the state_machines gem that
+        # the transition should be canceled.
+        #
+        # References:
+        #
+        #  - https://github.com/state-machines/state_machines/blob/309668998449ca6c348de809f34660d822bc626e/lib/state_machines/callback.rb#L81-L89
+        #  - https://github.com/state-machines/state_machines/blob/309668998449ca6c348de809f34660d822bc626e/lib/state_machines/transition_collection.rb#L63
         throw(:halt)
       end
     end
