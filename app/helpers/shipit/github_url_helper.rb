2ffdb52413dab95ad03dba39d2b5053c79b3c0c8
Jean byroot Boussier <jean.boussier+github@shopify.com>
2020-09-29 13:21:43 +0100

    Add review stacks changes (#76)
    * No need to path JS String
    
    * Fix typos and improve style in repository settings page
    
    * Get rid of an unecessary .presence call
    
    * Remove MergeRequestAssigment model
    
    * Refactor ProvisioningHandler to be less defensive
    
    * Lint attr_accessor
    
    * Fix duplicate html ID in repo settings.html
    
    Co-authored-by: Jean Boussier <jean.boussier@gmail.com>

--- app/helpers/shipit/github_url_helper.rb
+++ app/helpers/shipit/github_url_helper.rb
@@ -52,7 +52,7 @@ module Shipit
     end
 
     def stack_github_url(stack)
-      if stack.respond_to?(:pull_request) && stack.pull_request.presence
+      if stack.respond_to?(:pull_request) && stack.pull_request
         github_pull_request_url(stack.pull_request)
       else
         github_repo_url(stack.repo_owner, stack.repo_name)
