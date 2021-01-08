08308c027e7af1f44667aac110f6b2260199cf01
eldad <eldad@vcita.com>
2021-01-08 14:46:30 +0200

    predictive_build_task vuew

--- app/models/shipit/predictive_branch_task.rb
+++ app/models/shipit/predictive_branch_task.rb
@@ -9,6 +9,10 @@ module Shipit
 
     # delegate :broadcast_update, to: :stack
 
+    def title
+      self.class.name
+    end
+
     def report_complete!
       super
       predictive_branch.update_status(self )
