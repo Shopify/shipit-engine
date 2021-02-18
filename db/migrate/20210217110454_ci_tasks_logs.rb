0cd6d114aff539d5d294e82080364903b21977a5
Aviya Vaitzman <aviyavai@gmail.com>
2021-02-18 11:20:17 +0200

    Adding new table for managing ci jobs statuses. Manage ci jobs statuses

---
+++ db/migrate/20210217110454_ci_tasks_logs.rb
@@ -0,0 +1,17 @@
+class ShipitPipelineChanges < ActiveRecord::Migration[6.0]
+
+  def change
+
+    create_table :ci_jobs_statuses do |t|
+      t.references :predictive_build,               foreign_key: true, type: :bigint
+      t.references :predictive_branch,               foreign_key: true, type: :bigint
+      t.string    "name",     limit: 100,       null: false
+      t.string    "status",   limit: 50,        default: "running",   null: false
+      t.text      "link",     limit: 16777215,  null: false
+      t.timestamps
+    end
+
+  end
+
+end
+
