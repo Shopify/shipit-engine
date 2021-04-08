e53f201f1ae785ed825b3f5163c9ff40fa65b812
Aviya Vaitzman <aviyavai@gmail.com>
2021-04-08 16:56:48 +0300

    some fixes

--- db/migrate/20210408150453_merge_requests_new_index.rb
+++ db/migrate/20210408150453_merge_requests_new_index.rb
@@ -1,4 +1,4 @@
-class CiJobsStatuses < ActiveRecord::Migration[6.0]
+class MergeRequestsNewIndex < ActiveRecord::Migration[6.0]
 
   def change
     add_index :merge_requests, [:stack_id, :merge_status, :head_id], name: :index_status_head
