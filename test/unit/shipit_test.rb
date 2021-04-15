e02afe4b990fc43189c80cb14f8f7462a2838747
Riley Shott <riley.shott@shopify.com>
2021-04-14 18:47:16 -0700

    Add indifferent access support for searching through Github applications

--- test/unit/shipit_test.rb
+++ test/unit/shipit_test.rb
@@ -7,6 +7,20 @@ module Shipit
       Shipit.instance_variables.each(&Shipit.method(:remove_instance_variable))
     end
 
+    test ".github uses indifferent access to search through the Github applications" do
+      secrets = ActiveSupport::OrderedOptions.new
+      secrets.merge!(Rails::Secrets.parse(['test/dummy/config/secrets.yml'], env: Rails.env))
+      secrets.merge!(YAML.load_file('test/dummy/config/secrets_double_github_app.yml'))
+      secrets.deep_symbolize_keys!
+      Shipit.stubs(:secrets).returns(secrets)
+      assert_instance_of(Shipit::GitHubApp, Shipit.github(organization: 'OrgOne'))
+      assert_instance_of(Shipit::GitHubApp, Shipit.github(organization: :OrgOne))
+      assert_instance_of(Shipit::GitHubApp, Shipit.github(organization: 'orgone'))
+      assert_instance_of(Shipit::GitHubApp, Shipit.github(organization: :orgone))
+      assert_instance_of(Shipit::GitHubApp, Shipit.github(organization: :OrgTwo))
+      Shipit.unstub(:secrets)
+    end
+
     test ".github_teams returns an empty array if there's no team" do
       assert_equal([], Shipit.github_teams)
     end
