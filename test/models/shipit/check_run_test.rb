ff4300326a981b9036df3565b719352bbb75c114
Adam Shepley <adam.shepley@shopify.com>
2021-05-04 17:54:46 -0400

    Add tests for different checkrun update scenarios.
    Update merge request checkrun refresh test fields.
    
    Added test for checkrun timestamp refreshes.

--- test/models/shipit/check_run_test.rb
+++ test/models/shipit/check_run_test.rb
@@ -9,13 +9,65 @@ module Shipit
       @check_run = shipit_check_runs(:second_pending_travis)
     end
 
-    test ".create_or_update_from_github! is idempotent" do
+    test ".create_or_update_from_github! updates successfully" do
+      checkrun_time = Time.now
       assert_difference -> { @commit.check_runs.count }, +1 do
-        @commit.check_runs.create_or_update_from_github!(@stack.id, github_check_run)
+        @commit.check_runs.create_or_update_from_github!(
+          @stack.id,
+          github_check_run(conclusion: nil, checkrun_time: '2021-04-29T18:05:12Z')
+        )
+      end
+
+      assert_no_enqueued_jobs(only: RefreshCheckRunsJob) do
+        @commit.check_runs.create_or_update_from_github!(
+          @stack.id,
+          github_check_run(conclusion: 'success', checkrun_time: checkrun_time + 1.minute)
+        )
+      end
+
+      assert_equal 'success', @commit.check_runs.last.conclusion
+    end
+
+    test ".create_or_update_from_github! is idempotent" do
+      checkrun_time = Time.now
+      assert_difference -> { @commit.check_runs.count }, +1 do
+        @commit.check_runs.create_or_update_from_github!(@stack.id, github_check_run(checkrun_time: checkrun_time))
       end
 
       assert_no_difference -> { @commit.check_runs.count } do
-        @commit.check_runs.create_or_update_from_github!(@stack.id, github_check_run)
+        assert_no_enqueued_jobs(only: RefreshCheckRunsJob) do
+          @commit.check_runs.create_or_update_from_github!(@stack.id, github_check_run(checkrun_time: checkrun_time))
+        end
+      end
+    end
+
+    test ".create_or_update_from_github! enqueues refresh when new statuses have stale timestamps" do
+      checkrun_time = Time.now
+      assert_difference -> { @commit.check_runs.count }, +1 do
+        @commit.check_runs.create_or_update_from_github!(
+          @stack.id,
+          github_check_run(conclusion: 'success', checkrun_time: checkrun_time)
+        )
+      end
+
+      assert_no_difference -> { @commit.check_runs.count } do
+        assert_enqueued_with(job: RefreshCheckRunsJob) do
+          @commit.check_runs.create_or_update_from_github!(
+            @stack.id,
+            github_check_run(conclusion: nil, checkrun_time: checkrun_time - 1.minute)
+          )
+        end
+      end
+    end
+
+    test ".create_or_update_from_github! enqueues refresh when new statues have no timestamps" do
+      assert_no_difference -> { @commit.check_runs.count } do
+        assert_enqueued_with(job: RefreshCheckRunsJob, args: [stack_id: @stack.id]) do
+          @commit.check_runs.create_or_update_from_github!(
+            @stack.id,
+            github_check_run(conclusion: nil, checkrun_time: nil)
+          )
+        end
       end
     end
 
@@ -36,16 +88,17 @@ module Shipit
 
     private
 
-    def github_check_run
-      @github_check_run ||= OpenStruct.new(
+    def github_check_run(conclusion: 'success', checkrun_time: Time.now)
+      OpenStruct.new(
         id: 424_242,
-        conclusion: 'success',
+        conclusion: conclusion,
         output: OpenStruct.new(
           description: 'This is a description',
         ),
         name: 'Test Suite',
         html_url: 'http://example.com/run',
         details_url: 'http://example.com/details',
+        completed_at: checkrun_time,
       )
     end
   end
