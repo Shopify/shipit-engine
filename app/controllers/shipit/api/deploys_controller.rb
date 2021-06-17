9c9a1a37e0902e21563fd44b8e8d68728b8068df
Steph Sachrajda <steph.sachrajda@shopify.com>
2021-06-17 15:28:06 -0600

    add param and tests

--- app/controllers/shipit/api/deploys_controller.rb
+++ app/controllers/shipit/api/deploys_controller.rb
@@ -11,11 +11,13 @@ module Shipit
       params do
         requires :sha, String, length: { in: 6..40 }
         accepts :force, Boolean, default: false
+        accepts :require_ci, Boolean, default: false
         accepts :env, Hash, default: {}
       end
       def create
         commit = stack.commits.by_sha(params.sha) || param_error!(:sha, 'Unknown revision')
         param_error!(:force, "Can't deploy a locked stack") if !params.force && stack.locked?
+        param_error!(:require_ci, "Commit is not deployable") if params.require_ci && !commit.deployable?
         deploy = stack.trigger_deploy(commit, current_user, env: params.env, force: params.force)
         render_resource(deploy, status: :accepted)
       end
