16d70de997268c8043dd00a504a1b1e026107847
Erik Ryhorchuk <erik.ryhorchuk@shopify.com>
2022-02-11 07:55:43 -0700

    allow nil deploy url

--- test/controllers/api/stacks_controller_test.rb
+++ test/controllers/api/stacks_controller_test.rb
@@ -96,6 +96,24 @@ module Shipit
         assert_equal 'test', @stack.branch
       end
 
+      test "#update updates the stack when nil deploy_url" do
+        @stack.update(deploy_url: nil)
+        @stack.update(continuous_deployment: true)
+        assert_nil @stack.deploy_url
+        assert @stack.continuous_deployment
+
+        patch :update, params: {
+          id: @stack.to_param,
+          continuous_deployment: false,
+        }
+
+        assert_response :ok
+        @stack.reload
+
+        assert_nil @stack.deploy_url
+        refute @stack.continuous_deployment
+      end
+
       test "#index returns a list of stacks" do
         stack = Stack.last
         get :index
