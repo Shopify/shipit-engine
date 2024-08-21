1b9dd2540a32778cf5350f8829dad9f2847676ff
David Cornu <david@usepatch.com>
2024-08-20 20:44:45 -0400

    Add logic to interact with the schedule

--- app/models/shipit/continuous_delivery_schedule.rb
+++ app/models/shipit/continuous_delivery_schedule.rb
@@ -4,6 +4,53 @@ module Shipit
   class ContinuousDeliverySchedule < Record
     belongs_to(:stack)
 
-    DAYS = %w[sunday monday tuesday wednesday thursday friday saturday]
+    DAYS = %w[sunday monday tuesday wednesday thursday friday saturday].freeze
+
+    DeploymentWindow = Struct.new(:starts_at, :ends_at, :enabled) do
+      alias_method :enabled?, :enabled
+    end
+
+    def can_deploy?(now = Time.current)
+      # Make sure time is in the default time zone so weekdays match what is
+      # stored in the database.
+      now = now.in_time_zone(Time.zone)
+
+      deployment_window = get_deployment_window(now.to_date)
+
+      deployment_window.enabled? &&
+        now >= deployment_window.starts_at &&
+        now <= deployment_window.ends_at
+    end
+
+    def get_deployment_window(date)
+      wday_name = DAYS.fetch(date.wday)
+
+      enabled = read_attribute("#{wday_name}_enabled")
+
+      starts_at, ends_at = [:start, :end].map do |bound|
+        raw_time = read_attribute("#{wday_name}_#{bound}")
+
+        # `ActiveRecord::Type::Time` attributes are stored as timestamps
+        # normalized to 2000-01-01 so they can't be used for comparisons without
+        # having their dates adjusted.
+        # https://github.com/rails/rails/blob/ec667e5f114df58087493096253541f1034815af/activemodel/lib/active_model/type/time.rb#L23
+        Time.zone.local(
+          date.year,
+          date.month,
+          date.day,
+          raw_time.hour,
+          raw_time.min,
+        )
+      end
+
+      DeploymentWindow.new(
+        starts_at,
+        # Includes the full minute in the configured range. This is required so
+        # that a window configured to end at 17:59 actually ends at 17:59:59
+        # instead of 17:59:00.
+        ends_at.at_end_of_minute,
+        enabled,
+      )
+    end
   end
 end
--- test/models/shipit/continuous_delivery_schedule_test.rb
+++ test/models/shipit/continuous_delivery_schedule_test.rb
@@ -3,5 +3,72 @@ require "test_helper"
 
 module Shipit
   class ContinuousDeliveryScheduleTest < ActiveSupport::TestCase
+    test "defaults to all the time" do
+      schedule = Shipit::ContinuousDeliverySchedule.new
+
+      Shipit::ContinuousDeliverySchedule::DAYS.each_with_index do |day|
+        assert(schedule.read_attribute("#{day}_enabled"))
+
+        day_start = schedule.read_attribute("#{day}_start")
+        assert_equal(day_start.at_beginning_of_day, day_start)
+
+        day_end = schedule.read_attribute("#{day}_end")
+        assert_equal(day_end.at_end_of_day.at_beginning_of_minute, day_end)
+      end
+    end
+
+    test "#get_deployment_window" do
+      schedule = Shipit::ContinuousDeliverySchedule.new(
+        monday_enabled: false,
+        monday_start: "09:15",
+        monday_end: "17:30",
+      )
+
+      monday = Date.current.monday
+
+      deployment_window = schedule.get_deployment_window(monday)
+
+      refute(deployment_window.enabled?)
+
+      starts_at = deployment_window.starts_at
+      assert_equal(monday, starts_at.to_date)
+      assert_equal(9, starts_at.hour)
+      assert_equal(15, starts_at.min)
+      assert_equal(starts_at.at_beginning_of_minute, starts_at)
+
+      ends_at = deployment_window.ends_at
+      assert_equal(monday, ends_at.to_date)
+      assert_equal(17, ends_at.hour)
+      assert_equal(30, ends_at.min)
+      assert_equal(ends_at.at_end_of_minute, ends_at)
+    end
+
+    test "#can_deploy? is false if the day is disabled" do
+      schedule = Shipit::ContinuousDeliverySchedule.new(
+        tuesday_enabled: false,
+        tuesday_start: "00:00",
+        tuesday_end: "23:59",
+      )
+
+      tuesday = Date.current.monday.advance(days: 1).beginning_of_day
+
+      refute(schedule.can_deploy?(tuesday))
+    end
+
+    test "#can_deploy? is true when the current time is within the window" do
+      schedule = Shipit::ContinuousDeliverySchedule.new(
+        wednesday_enabled: true,
+        wednesday_start: "09:15",
+        wednesday_end: "17:30",
+      )
+
+      wednesday = Date.current.monday.advance(days: 2).beginning_of_day
+
+      refute(schedule.can_deploy?(wednesday))
+      assert(schedule.can_deploy?(wednesday.advance(hours: 9, minutes: 15)))
+      assert(schedule.can_deploy?(wednesday.advance(hours: 12)))
+      assert(schedule.can_deploy?(wednesday.advance(hours: 17, minutes: 30).at_end_of_minute))
+      refute(schedule.can_deploy?(wednesday.advance(hours: 17, minutes: 31)))
+    end
   end
 end
