017da9f42f4c0fafe9b0edeaf48dfea30d391975
David Cornu <david@usepatch.com>
2024-08-24 14:22:56 -0400

    Wire schedule into `ContinuousDeliveryJob`

--- app/jobs/shipit/continuous_delivery_job.rb
+++ app/jobs/shipit/continuous_delivery_job.rb
@@ -9,6 +9,12 @@ module Shipit
     def perform(stack)
       return unless stack.continuous_deployment?
 
+      # If there is a schedule defined for this stack, make sure we are within a
+      # deployment window before proceeding.
+      if stack.continuous_delivery_schedule
+        return unless stack.continuous_delivery_schedule.can_deploy?
+      end
+
       # checks if there are any tasks running, including concurrent tasks
       return if stack.occupied?
 
